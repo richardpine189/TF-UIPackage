85ab334b966901134987c44ee2539f41bd7dcedc
RichardPine <73888797+richardpine189@users.noreply.github.com>
2024-08-25 21:25:17 -0300

    Update PackageInitializer.cs

--- Editor/PackageInitializer.cs
+++ Editor/PackageInitializer.cs
@@ -7,7 +7,6 @@ public class PackageInitializer
 {
     static PackageInitializer()
     {
-        // Esto se ejecuta cuando se importa el paquete
         EditorApplication.update += OnFirstLoad;
     }
 
@@ -15,7 +14,6 @@ public class PackageInitializer
     {
         EditorApplication.update -= OnFirstLoad;
 
-        // Verifica si los archivos ya están importados
         string pathToCheck = "Assets/Resources";
         if (!Directory.Exists(pathToCheck))
         {
@@ -29,14 +27,12 @@ public class PackageInitializer
 
     private static void ImportAdditionalFiles()
     {
-        // Define la ruta a los archivos que quieres copiar
+        
         string packagePath = "Packages/com.ricardopino.uipackage/Assets";
         string targetPath = "Assets";
-
-        // Copia los archivos
+        
         FileUtil.CopyFileOrDirectory(packagePath, targetPath);
-
-        // Refresca el proyecto para que Unity reconozca los nuevos archivos
+        
         AssetDatabase.Refresh();
 
         Debug.Log("Archivos adicionales importados exitosamente.");
